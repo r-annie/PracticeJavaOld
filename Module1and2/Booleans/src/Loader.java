d8e3478e407ab6eec6074ea142a80f7b58156efd
ranilock <ranilockwood@list.ru>
2021-08-24 18:32:02 +0400

    Input parameters

--- Module1and2/Booleans/src/Loader.java
+++ Module1and2/Booleans/src/Loader.java
@@ -1,31 +1,40 @@
+import java.util.Scanner;
 
 public class Loader {
     public static void main(String[] args) {
-        int milkAmount = 1200; // ml
-        int powderAmount = 1200; // g
-        int eggsCount = 1200; // items
-        int sugarAmount = 1200; // g
-        int oilAmount = 1200; // ml
-        int appleCount = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input milk amount (ml): ");
+        int milkAmount = in.nextInt();
+        System.out.print("Input powder amount (g): ");
+        int powderAmount = in.nextInt();
+        System.out.print("Input eggs count (items): ");
+        int eggsCount = in.nextInt();
+        System.out.print("Input sugar amount (g): ");
+        int sugarAmount = in.nextInt();
+        System.out.print("Input oil amount (ml): ");
+        int oilAmount = in.nextInt();
+        System.out.print("Input apple count (items): ");
+        int appleCount = in.nextInt();
+
         boolean canCookAnything = false;
         boolean pancakesCook = powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30;
         boolean omeletteCook = milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5;
         boolean applepieCook = appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4;
-
-        if (pancakesCook == true) {
+        
+        if (pancakesCook) {
 
             // powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
             System.out.println("Pancakes");
             canCookAnything = true;
         }
 
-        if (omeletteCook == true) {
+        if (omeletteCook) {
             //milk - 300 ml, powder - 5 g, eggs - 5
             System.out.println("Omelette");
             canCookAnything = true;
         }
 
-        if (applepieCook == true) {
+        if (applepieCook) {
             //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
             System.out.println("Apple pie");
             canCookAnything = true;
