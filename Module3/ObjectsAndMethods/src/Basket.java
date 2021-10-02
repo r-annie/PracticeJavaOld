098944716fed9897fb6b6b241972917a6f239d21
ranilock <ranilockwood@list.ru>
2021-10-02 21:15:08 +0400

    First task added

---
+++ Module3/ObjectsAndMethods/src/Basket.java
@@ -0,0 +1,86 @@
+public class Basket {
+
+    private static int count = 0;
+    private String items = "";
+    private int totalPrice = 0;
+    private int limit;
+    private double totalWeight = 0;
+
+    public Basket() {
+        increaseCount(1);
+        this.limit = 1000000;
+    }
+
+    public Basket(int limit) {
+        this();
+        this.limit = limit;
+    }
+
+    public Basket(String items, int totalPrice) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void increaseCount(int count) {
+        Basket.count = Basket.count + count;
+    }
+
+    public void add(String name, int price) {
+        add(name, price, 1, 0);
+    }
+
+    public void add(String name, int price, int count) { add(name, price, count, 0);}
+
+    public void add(String name, int price, int count, double weight) {
+        boolean error = false;
+        if (contains(name)) {
+            error = true;
+        }
+
+        if (totalPrice + count * price >= limit) {
+            error = true;
+        }
+
+        if (error) {
+            System.out.println("Error occured :(");
+            return;
+        }
+
+        items = items + "\n" + name + " - " +
+            count + " шт. - " + price + " руб. - " + weight + " кг, вес 1 шт. ";
+        totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
+    }
+
+    public double getTotalWeight () {
+        return totalWeight;
+    }
+
+    public void clear() {
+        items = "";
+        totalPrice = 0;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public boolean contains(String name) {
+        return items.contains(name);
+    }
+
+    public void print(String title) {
+        System.out.println("\n" + title);
+        if (items.isEmpty()) {
+            System.out.print("Корзина пуста");
+        } else {
+            System.out.print("Список товаров:");
+            System.out.println(items);
+        }
+    }
+}
---
+++ Module3/ObjectsAndMethods/src/Main.java
@@ -0,0 +1,23 @@
+import javax.sound.midi.Soundbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basketPetya = new Basket();
+        basketPetya.add("Milk", 40);
+        basketPetya.add("Chocolate", 100);
+        basketPetya.print("Корзина Пети:");
+
+        Basket basketLily = new Basket();
+        basketLily.add("Ice cream", 20, 3);
+        basketLily.add("Crisps", 75,2,0.05);
+        basketLily.add("Cola", 50,3,0.5);
+        basketLily.print("Корзина Лили:");
+        System.out.println("Общая стоимость: " + basketLily.getTotalPrice() + " руб.");
+        System.out.println("Общий вес: " + basketLily.getTotalWeight() + " кг");
+
+
+        Basket basketMiku = new Basket();
+        basketMiku.print("Корзина Мику:");
+    }
+}
